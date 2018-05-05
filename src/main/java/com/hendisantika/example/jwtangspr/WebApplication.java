5a44a09e7aad7a56cd77a2b4384952bed2e255f7
Hendi Santika <hendisantika@gmail.com>
2018-05-05 07:28:32 +0700

    #2 - Upgrade to Spring Boot 1.5.12.RELEASE version

---
+++ src/main/java/com/hendisantika/example/jwtangspr/WebApplication.java
@@ -0,0 +1,27 @@
+package com.hendisantika.example.jwtangspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+public class WebApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/*");
+
+        return registrationBean;
+    }
+
+}
